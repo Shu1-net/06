f48d0510cbbbc88995ca2313d73c1c5da7c8c3fe
Shu1-net <shuichi.ama@gmail.com>
2020-08-25 23:25:01 +0900

    second  commit

--- index.php
+++ index.php
@@ -1,47 +1,93 @@
 <?php
 
-// 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
+// 以下にItemクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
+class Item  
+{
+    private $name;
+    private $number;
+    private $color;
 
+    public function __construct($name, $number, $color)
+    {
+        $this->name = $name;
+        $this->number = $number;
+        $this->color = $color;
+    }
 
+    public function getName()
+    {
+        return $this->name;
+    }
 
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function infomation()
+    {
+        return '品名:' . $this->name . '、サイズ番号:' . $this->number .
+            '、カラー:' . $this->color . 'です。';
+    }
+}
 
 
 // 以下を実行した時にエラーがでないようにして下さい。
 // Carクラスのインスタンスを生成
-// 引数 : 車名、車体番号、カラー
-$car1 = new Car('ランクル', 100, 'black');
+// 引数 : 品名、サイズ番号、カラー
+$item1 = new Item ('Denim', 32, 'Cheakerboard');
 
-// 車名(車種)を取得
-echo $car1->getName();
+// 品名を取得
+echo $item1->getName();
 echo '<br>';
 
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
-
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// サイズ番号を取得
+echo $item1->getNumber();
 echo '<br>';
 
 // カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
+echo $item1->getColor();
 echo '<br>';
 
-// 車の情報表示を表示
-echo $car1->infomation();
+// アイテムの情報表示を表示
+echo $item1->infomation();
+echo '<br>';
+
+// 品名を更新
+$item1->setName('Jeans');
+echo $item1->getName();
+echo '<br>';
+
+// サイズ番号を取得
+$item1->setNumber(200);
+echo $item1->getNumber();
+echo '<br>';
+
+// カラーを取得
+$item1->setColor('red');
+echo $item1->getColor();
+echo '<br>';
+
+// 商品の情報表示を表示
+echo $item1->infomation();
 echo '<br>';
